0933a82c890b90337db6c55d704263bec3adf51f
TaNJIepDePd3H <armanly06@bk.ru>
2025-04-08 00:17:20 +0600

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,9 +1,17 @@
 function upDate(previewPic) {
-    document.getElementById('image').style.backgroundImage = "url('" + previewPic.src + "')";
-    document.getElementById('image').innerHTML = previewPic.alt;
+    // Get the main image div
+    var mainImage = document.getElementById('image');
+    
+    // Change the background image and text of the div
+    mainImage.style.backgroundImage = "url('" + previewPic.src + "')";
+    mainImage.innerHTML = previewPic.alt;
 }
 
 function unDo() {
-    document.getElementById('image').style.backgroundImage = "url('')";
-    document.getElementById('image').innerHTML = "Hover over an animal below to display here.";
+    // Get the main image div
+    var mainImage = document.getElementById('image');
+    
+    // Reset the background image and text
+    mainImage.style.backgroundImage = "none";
+    mainImage.innerHTML = "Hover over an image below to display here.";
 }
